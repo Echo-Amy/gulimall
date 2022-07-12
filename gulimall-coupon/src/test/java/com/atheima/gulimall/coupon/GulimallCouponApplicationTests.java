1fb20404bd834f93541e8e944a9dbfe15c07a38f
Echo-Amy <e0674573@u.nus.edu>
2022-07-12 21:48:00 +0800

    Initial Structure Build

---
+++ gulimall-coupon/src/test/java/com/atheima/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atheima.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
